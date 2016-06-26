c30ca583c1fd2631aa76bc36115c260121c40f4f
Zack Berman <zack_berman@Zacharys-MacBook-Pro.local>
2016-06-26 13:03:33 -0500

    scrape.py now fetches data from RV, CV, NP

--- scrape.py
+++ scrape.py
@@ -3,15 +3,27 @@ import json
 import re
 from bs4 import BeautifulSoup
 
-web = urllib.urlopen("https://www.irvinecompanyapartments.com/communities/river-view/prices-and-floorplans")
-pattern = re.compile('var FloorPlanData = (.*?);')
+urls = {"RiverView" : "https://www.irvinecompanyapartments.com/communities/river-view/prices-and-floorplans",
+        "CrescentVillage" : "https://www.irvinecompanyapartments.com/communities/crescent-village/prices-and-floorplans",
+        "NorthPark" : "https://www.irvinecompanyapartments.com/communities/north-park/prices-and-floorplans",}
 
-soup = BeautifulSoup(web.read(), "lxml")
-scripts = soup.find_all('script')
-json_data = None
-for script in scripts:
-   if(pattern.match(str(script.string))):
-       data = pattern.match(script.string)
-       json_data = json.loads(data.groups()[0])
+for name, url in urls.iteritems():
+    print "\n", name
+    web = urllib.urlopen(url)
+    pattern = re.compile('var FloorPlanData = (.*?);')
 
-print json_data[0]['Id']
+    soup = BeautifulSoup(web.read(), "lxml")
+    scripts = soup.find_all('script')
+    json_data = None
+    for script in scripts:
+       if(pattern.match(str(script.string))):
+           data = pattern.match(script.string)
+           json_data = json.loads(data.groups()[0])
+
+    fields = ['PlanName', 'SquareFeet', 'MinimumRent', 'MaximumRent']
+
+    for plan in json_data:
+        if plan['PlanType'] == 'Two Bedrooms' and plan['AvailableUnits'] is not False:
+            print ""
+            for field in fields:
+                print field, ':', plan[field]
