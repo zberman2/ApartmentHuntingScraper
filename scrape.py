8b59186325666ba86882fd3292111c7a261dfdec
Zack Berman <zack_berman@Zacharys-MacBook-Pro.local>
2016-06-26 12:43:51 -0500

    Initial commit of scrape.py

---
+++ scrape.py
@@ -0,0 +1,20 @@
+from lxml import html
+import requests
+import webbrowser
+import urllib
+import json
+import re
+from bs4 import BeautifulSoup
+
+web = urllib.urlopen("https://www.irvinecompanyapartments.com/communities/river-view/prices-and-floorplans")
+pattern = re.compile('var FloorPlanData = (.*?);')
+
+soup = BeautifulSoup(web.read(), "lxml")
+scripts = soup.find_all('script')
+json_data = None
+for script in scripts:
+   if(pattern.match(str(script.string))):
+       data = pattern.match(script.string)
+       json_data = json.loads(data.groups()[0])
+
+print json_data[0]['Id']
